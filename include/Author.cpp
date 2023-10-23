6d7db9e5766c88fab89671927ac0eec7f6f38a63
Dev <devpratap2321@gmail.com>
2023-10-23 22:55:22 +0530

    Array of Objects and Static Elements

--- include/Author.cpp
+++ include/Author.cpp
@@ -1,18 +1,23 @@
 #include "Author.h"
 
+int Author::author_count = 0; // Initialize the static member variable
+
 Author::Author(int author_id, std::string author_name) {
     this->author_id = author_id;
     this->author_name = author_name;
     std::cout << "Parameterized constructor called" << std::endl;
+    author_count++;
 }
 
 Author::Author(const Author &other)
     : author_id(other.author_id), author_name(other.author_name) {
-    std::cout << "Copy constructor called" << std::endl;
+    std::cout << "Author Copy constructor called" << std::endl;
+    author_count++;
 }
 
 Author::~Author() {
     std::cout << "Author Destructor called" <<  std::endl;
+    author_count--;
 }
 
 void Author::display() {
@@ -20,3 +25,6 @@ void Author::display() {
     std::cout << this->author_name << std::endl;
 }
 
+void Author::getAuthorCount() {
+    std::cout << "Total Number of Authors: " << author_count << std::endl;
+}
--- include/Member.cpp
+++ include/Member.cpp
@@ -1,15 +1,19 @@
 #include "Member.h"
 
+int Member::member_count = 0;
+
 Member::Member(int id, std::string name, std::string email, std::string password)
     : User(id, name, email, password) {
     this->id = id;
     this->name = name;
     this->email = email;
     this->password = password;
+    member_count++;
 }
 
 Member::~Member() {
     std::cout << "Member Destructor called" << std::endl;
+    member_count--;
 }
 
 void Member::Login() {
@@ -22,5 +26,9 @@ void Member::Logout() {
 
 void Member::Register() {
    
+}
+
+void Member::getMemberCount() {
+    std::cout << "Total number of Members: " << member_count << std::endl;
 }
 
